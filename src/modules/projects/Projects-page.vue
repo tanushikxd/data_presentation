e31fa08b2df1faf965a4bbdd91fde59d6a8ee31e
tanushikxd <tanushikxdt2012@gmail.com>
2025-10-26 15:26:42 +0000

    added a project mods

--- src/modules/projects/Projects-page.vue
+++ src/modules/projects/Projects-page.vue
@@ -1,13 +1,32 @@
 <template>
-  <section class="projects">
-    <div class="projects_title">
-      <h1 class="projects_title-name">My projects</h1>
+  <div class="projects">
+    <h1>My project</h1>
+    <div class="projects-list">
+      <div v-for="project in projects" :key="project.id" class="project-card">
+        <h2>{{ project.title }}</h2>
+        <p>{{ project.description }}</p>
+        <a :href="project.link" target="_blank">Open in github</a>
+      </div>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "ProjectsPage",
+  data() {
+    return {
+      projects: [
+        {
+          id: 1,
+          title: "Superstore data",
+          description: "Different tools been used for this project",
+          link: "https://github.com/tanushikxd/data_presentation_visuals",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
